2e06de8bde77b8842b14c7c5fb815ab5cb5d722c
GitubSunwei <18969653044@163.com>
2020-11-20 11:13:50 +0800

    掌握技能提交

---
+++ html5/data.js
@@ -0,0 +1,45 @@
+// 1000w
+// 一月份     200         200/1000
+// 二月份     150         150/1000
+// 三月份     260         260/1000
+// 四月份     240         240/1000
+// 五月份     30          30/1000
+// 六月份     120         120/1000
+var data = [
+    {
+        "month":"一月份",
+        "money":"200",
+        "persent": 200/1000,
+        "color":"blue"
+    },
+    {
+        "month":"二月份",
+        "money":"150",
+        "persent": 150/1000,
+        "color":"orange"
+    },
+    {
+        "month":"三月份",
+        "money":"260",
+        "persent": 260/1000,
+        "color":"purple"
+    },
+    {
+        "month":"四月份",
+        "money":"240",
+        "persent": 240/1000,
+        "color":"pink"
+    },
+    {
+        "month":"五月份",
+        "money":"30",
+        "persent": 30/1000,
+        "color":"green"
+    },
+    {
+        "month":"六月份",
+        "money":"120",
+        "persent": 120/1000,
+        "color":"yellow"
+    }
+]
